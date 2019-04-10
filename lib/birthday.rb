a65adf0393162dbce29065ba81fd29769b583b9f
han xu <hanxu27@gmail.com>
2019-04-10 20:06:46 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -11,4 +11,10 @@ def happy_birthday(birthday_kids)
   end
 end
 
+def happy_appropriate_birthday(birthday_kids)
+  # add your code snippet here!
+  birthday_kids.each do |name, age|
+    puts "Happy Birthday #{name}! You are now #{age} years old!"
+  end
+end
 
