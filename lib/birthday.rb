f8cc3b82343c6c5cffc26278392f63a659761bd2
han xu <hanxu27@gmail.com>
2019-04-10 20:08:04 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -11,7 +11,7 @@ def happy_birthday(birthday_kids)
   end
 end
 
-def happy_appropriate_birthday(birthday_kids)
+def age_appropriate_birthday(birthday_kids)
   # add your code snippet here!
   birthday_kids.each do |name, age|
     if age < 13
