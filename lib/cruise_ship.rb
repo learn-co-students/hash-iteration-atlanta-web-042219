6f23c25656ba48fc2793fb8046bf9d70c3cb9e67
Andrew Allen <asallen67@gmail.com>
2019-04-10 23:22:06 -0500

    1

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -1,11 +1,6 @@
-# passengers = {
-# suite_a: "Amanda Presley", 
-# suite_b: "Seymour Hoffman", 
-# suite_c: "Alfred Tennyson", 
-# suite_d: "Charlie Chaplin", 
-# suite_e: "Crumpet the Elf"
-# }
-
-def select_winner(passengers)
-  # add the code snippet here!
+def select_winner(hsh)
+	hsh.each { |n|
+		return n[1] if n[0]==:suite_a && n[1][0]=="A"
+	}
+	"No winner found."
 end
